4f092ca46b8ab4708a78567cae8682a514e2f488
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-04-26 19:05:16 -0400

    Ignorar arquivos de build e instalação

--- ros2_marvin/build/marvin_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/debora/IARTES/ProjFinal/ros2_marvin/src/marvin_bringup
-# Build directory: /home/debora/IARTES/ProjFinal/ros2_marvin/build/marvin_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
