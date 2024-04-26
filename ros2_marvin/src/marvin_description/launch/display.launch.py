9460ff7dc43174f786ca3d3533d45e0a940d1074
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-04-26 18:51:07 -0400

    Corrigindo argumento do nó RViz para usar configuração definida no lançamento

--- ros2_marvin/src/marvin_description/launch/display.launch.py
+++ ros2_marvin/src/marvin_description/launch/display.launch.py
@@ -7,19 +7,23 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
+    # Obtém o caminho do arquivo URDF
     urdf_file = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         "..", "..", "marvin_description", "urdf", "marvin.urdf"
     )
 
+    # Lê o conteúdo do arquivo URDF
     with open(urdf_file, "r") as infp:
         robot_desc = infp.read()
 
+    # Obtém o caminho do arquivo de configuração RViz
     rviz_config = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         "..", "..", "marvin_description", "rviz", "config.rviz"
     )
 
+    # Define o nó do publicador de estado do robô
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
@@ -28,6 +32,7 @@ def generate_launch_description():
         parameters=[{"robot_description": robot_desc}],
     )
 
+    # Define o nó do publicador de estados conjuntos
     joint_state_publisher_node = Node(
         package="joint_state_publisher",
         executable="joint_state_publisher",
@@ -35,6 +40,7 @@ def generate_launch_description():
         condition=UnlessCondition(LaunchConfiguration("gui")),
     )
 
+    # Define o nó da interface gráfica do publicador de estados conjuntos
     joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
@@ -42,21 +48,24 @@ def generate_launch_description():
         condition=IfCondition(LaunchConfiguration("gui")),
     )
 
+    # Define o nó do RViz
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="screen",
-        arguments=["-d", rviz_config],  # Corrigido aqui para usar a variável rviz_config
+        arguments=["-d", LaunchConfiguration("rvizconfig")],  # Usa a configuração RViz
     )
 
     return LaunchDescription(
         [
+            # Declaração do argumento para habilitar a interface gráfica do publicador de estados conjuntos
             DeclareLaunchArgument(
                 name="gui",
                 default_value="True",
                 description="Flag to enable joint_state_publisher_gui",
             ),
+            # Declaração do argumento para o arquivo de configuração RViz
             DeclareLaunchArgument(
                 name="rvizconfig",
                 default_value=rviz_config,
@@ -68,5 +77,4 @@ def generate_launch_description():
             rviz_node,
         ]
     )
-
 
