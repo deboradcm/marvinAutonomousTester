5983d4d37511475d9c4f4b55e147726ab643da44
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-05-07 12:45:34 -0400

    Adionando nova descrição do robô

--- ros2_marvin/src/build/marvin_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/debora/IARTES/ProjFinal/ros2_marvin/src/install/mqtt_publish/share/mqtt_publish/package.sh"
-. "/home/debora/IARTES/ProjFinal/ros2_marvin/src/install/topic_to_terminal/share/topic_to_terminal/package.sh"
