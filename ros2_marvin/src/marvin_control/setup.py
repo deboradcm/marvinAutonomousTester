e6ae980c00a5cb244cb700e54a4d6efd42fcb036
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-05-08 15:22:03 -0400

    Adicionando ponto de entrada para o script de controle do Marvin.

--- ros2_marvin/src/marvin_control/setup.py
+++ ros2_marvin/src/marvin_control/setup.py
@@ -20,6 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'marvin_controller = marvin_control.marvin_controller:main'
         ],
     },
 )
