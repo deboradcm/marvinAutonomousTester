e76c354ed3bd409bf0a9dc8b3eb3ccbac52611aa
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-05-10 16:38:01 -0400

    Adicionando função para carregar arquivos YAML

--- ros2_marvin/src/marvin_bringup/launch/marvin_launch.py
+++ ros2_marvin/src/marvin_bringup/launch/marvin_launch.py
@@ -4,73 +4,92 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import SetEnvironmentVariable
 import xacro
 
+# LOAD YAML:
+def load_yaml(package_name, file_path):
+    package_path = get_package_share_directory(package_name)
+    absolute_file_path = os.path.join(package_path, file_path)
+    try:
+        with open(absolute_file_path, 'r') as file:
+            return yaml.safe_load(file)
+    except EnvironmentError:
+        # parent of IOError, OSError *and* WindowsError where available.
+        return None
+
+# ========== **GENERATE LAUNCH DESCRIPTION** ========== #
 def generate_launch_description():
-    ld = LaunchDescription()
     
-    pkg_name = 'marvin_description'  # Nome do pacote
+    # DECLARE Gazebo WORLD file:
+    world_path = os.path.join(
+        get_package_share_directory('marvin_description'),
+        'worlds',
+        'marvin.world')
+    
+    # DECLARE Gazebo LAUNCH file:
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+                launch_arguments={'world': world_path}.items(),
+             )
 
-    pkg_share = get_package_share_directory(pkg_name)
+    # PANDA ROBOT Description file package:
+    panda_description_path = os.path.join(
+        get_package_share_directory('marvin_description'))
+    
+    # PANDA ROBOT ROBOT urdf file path:
+    xacro_file = os.path.join(panda_description_path,
+                              'urdf',
+                              'ur3e_urdf.xacro')
+    
+    # Generate ROBOT_DESCRIPTION for PANDA ROBOT:
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc, mappings={"cell_layout_1": "true"})
+    robot_description_config = doc.toxml()
+    robot_description = {'robot_description': robot_description_config}
 
-    urdf_path = 'urdf/ur3e_urdf.xacro'
-
-    rviz_relative_path = 'marvin_bringup/rviz/urdf_config.rviz'
-
-    rviz_absolute_path = os.path.join(pkg_share, rviz_relative_path)
-
-    # Processamento do arquivo xacro usando o módulo xacro
-    xacro_file = os.path.join(pkg_share, urdf_path)
-    urdf_content = xacro.process_file(xacro_file).toxml()
-    robot_description = {'robot_description': urdf_content}
-    world_path = os.path.join(pkg_share, 'worlds', 'chesset.world')
-
-    robot_state_publisher_node = Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[robot_description],
-        output="screen"
+    # ROBOT STATE PUBLISHER NODE:
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
     )
-    
-    gazebo_launch_file = os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
-    gazebo_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([gazebo_launch_file]),
-        launch_arguments={'world': world_path}.items()
-    )
-    
-    spawn_entity_node = Node(
-        package="gazebo_ros",
-        executable="spawn_entity.py",
-        arguments=["-topic", "robot_description", "-entity", "my_robot"],
-        output="screen"
-    )
-    
+
+    # SPAWN ROBOT TO GAZEBO:
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'marvin'],
+                        output='screen')
+
+    # MQTT PUBLISHER NODE:
     mqtt_publisher_node = Node(
         package="mqtt_publish",
         executable="mqtt_publish",
         output="screen"
     )
     
+    # TOPIC TERMINAL NODE:
     topic_terminal_node = Node(
         package="topic_to_terminal",
         executable="terminal_sub",
         output="screen"
     )
     
+    # JOINT CONTROLLER NODE:
     joint_controller_node = Node(
         package="marvin_control",
         executable="marvin_controller",
         output="screen"
     )
 
-   
-    ld.add_action(mqtt_publisher_node)
-    ld.add_action(topic_terminal_node)
-    ld.add_action(robot_state_publisher_node)
-    ld.add_action(gazebo_launch)
-    ld.add_action(spawn_entity_node)
-    ld.add_action(joint_controller_node)
-    
-    return ld
+    # Adicionando todos os nós à descrição de lançamento
+    return LaunchDescription([
+        gazebo, 
+        node_robot_state_publisher,
+        spawn_entity,
+        mqtt_publisher_node,
+        topic_terminal_node,
+        joint_controller_node
+    ])
 
