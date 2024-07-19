885f3ced82cac88fc116c4d5ebe04a88ef2b6791
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-07-19 14:42:58 -0400

    Adiciona arquivos do projeto testAppMarvin

---
+++ testAppMarvin/app/src/main/java/com/example/marvin/Retorno.java
@@ -0,0 +1,22 @@
+package com.example.marvin;
+
+public class Retorno {
+    private String retorno;
+    private int id_robot;
+
+    public String getRetorno() {
+        return retorno;
+    }
+
+    public void setRetorno(String retorno) {
+        this.retorno = retorno;
+    }
+
+    public int getId_robot() {
+        return id_robot;
+    }
+
+    public void setId_robot(int id_robot) {
+        this.id_robot = id_robot;
+    }
+}
