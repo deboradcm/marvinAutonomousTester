0b405226ae138df8b30f481589f7f40ac931280f
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-07-22 16:01:23 -0400

    Refatorar CoordinateConverter.java para simplificação e conversão de metros para pixels
    - Removidos métodos e construtores desnecessários para simplificar a classe.
    - Adicionados novos campos para representar as dimensões da tela em metros e pixels.
    - Adicionado método convertMetersToPixels para converter coordenadas em metros para coordenadas em pixels.
    - Ajustada a lógica para garantir que as coordenadas em pixels considerem a inversão do eixo y, se necessário.

--- testAppMarvin/app/src/main/java/com/example/marvin/CoordinateConverter.java
+++ testAppMarvin/app/src/main/java/com/example/marvin/CoordinateConverter.java
@@ -1,59 +1,29 @@
 package com.example.marvin;
 
-import java.util.Random;
-
 public class CoordinateConverter {
-    private int screenWidth;
-    private int screenHeight;
-    private double x;
-    private double y;
-    private double z;
-    private double fov;
+    private double screenWidthInMeters;
+    private double screenHeightInMeters;
+    private int screenWidthInPixels;
+    private int screenHeightInPixels;
 
-    public CoordinateConverter(double x, double y, double z, int screenWidth, int screenHeight, double fov) {
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
-        this.x = x;
-        this.y = y;
-        this.z = z;
-        this.fov = fov;
+    public CoordinateConverter(double screenWidthInMeters, double screenHeightInMeters, int screenWidthInPixels, int screenHeightInPixels) {
+        this.screenWidthInMeters = screenWidthInMeters;
+        this.screenHeightInMeters = screenHeightInMeters;
+        this.screenWidthInPixels = screenWidthInPixels;
+        this.screenHeightInPixels = screenHeightInPixels;
     }
 
-    public CoordinateConverter(double x, double y, double z) {
-        this(x, y, z, 1080, 2220, 90);
-    }
+    public int[] convertMetersToPixels(double xInMeters, double yInMeters) {
+        double pixelsPerMeterX = screenWidthInPixels / screenWidthInMeters;
+        double pixelsPerMeterY = screenHeightInPixels / screenHeightInMeters;
 
-    public double[] generateRandomCoordinates(double xMin, double xMax, double yMin, double yMax, double zMin, double zMax) {
-        Random random = new Random();
-        double x = xMin + (xMax - xMin) * random.nextDouble();
-        double y = yMin + (yMax - yMin) * random.nextDouble();
-        double z = zMin + (zMax - zMin) * random.nextDouble();
-        return new double[]{x, y, z};
-    }
+        int pixelX = (int) (xInMeters * pixelsPerMeterX);
+        int pixelY = (int) (yInMeters * pixelsPerMeterY);
 
-    public int[] convertToPixelCoordinates() {
-        // Distância do plano de projeção
-        double d = screenWidth / (2 * Math.tan(Math.toRadians(fov) / 2));
+        // Assegure que pixelY é calculado de forma que o eixo y seja invertido se necessário
+        pixelY = screenHeightInPixels - pixelY;
 
-        // Projeção em perspectiva
-        double u = (x / z) * d;
-        double v = (y / z) * d;
-
-        // Converter para coordenadas de pixels
-        int pixelX = (int) (screenWidth / 2 + u);
-        int pixelY = (int) (screenHeight / 2 - v);
-
-        return new int[]{pixelX, pixelY};
-    }
-
-    public static void main(String[] args) {
-        CoordinateConverter converter = new CoordinateConverter(1.0, 2.0, 3.0);
-
-        // Exemplo de uso
-        double[] randomCoords = converter.generateRandomCoordinates(-10, 10, -10, 10, 1, 100);
-        System.out.println("Random Coordinates: x=" + randomCoords[0] + ", y=" + randomCoords[1] + ", z=" + randomCoords[2]);
-
-        int[] pixelCoords = converter.convertToPixelCoordinates();
-        System.out.println("Pixel Coordinates: x=" + pixelCoords[0] + ", y=" + pixelCoords[1]);
+        return new int[] { pixelX, pixelY };
     }
 }
+
