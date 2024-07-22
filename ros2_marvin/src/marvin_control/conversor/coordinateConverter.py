7ff6bafb1347363a8cd76c6311de9e0bf206297d
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-07-22 15:59:48 -0400

    Remover docstrings redundantes em CoordinateConverter.py
    - Removidos os docstrings dos métodos e do construtor para simplificar o código.
    - Mantida a funcionalidade original dos métodos generate_random_coordinates e convert_to_pixel_coordinates.

--- ros2_marvin/src/marvin_control/conversor/coordinateConverter.py
+++ ros2_marvin/src/marvin_control/conversor/coordinateConverter.py
@@ -5,19 +5,7 @@ class CoordinateConverter:
                  x_min=-35.495777, x_max=35.495777,
                  y_min=-3.872530, y_max=3.872530,
                  z_min=-71.830040, z_max=71.830040):
-        """
-        Inicializa a classe CoordinateConverter.
-        
-        Argumentos:
-            screen_width (int): Largura da tela em pixels.
-            screen_height (int): Altura da tela em pixels.
-            x_min (float): Valor mínimo da coordenada x.
-            x_max (float): Valor máximo da coordenada x.
-            y_min (float): Valor mínimo da coordenada y.
-            y_max (float): Valor máximo da coordenada y.
-            z_min (float): Valor mínimo da coordenada z.
-            z_max (float): Valor máximo da coordenada z.
-        """
+
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.x_min = x_min
@@ -28,29 +16,14 @@ class CoordinateConverter:
         self.z_max = z_max
 
     def generate_random_coordinates(self):
-        """
-        Gera coordenadas aleatórias x, y, z dentro do bounding box.
-        
-        Retorna:
-            tuple: Um tuple contendo as coordenadas (x, y, z).
-        """
+
         x = random.uniform(self.x_min, self.x_max)
         y = random.uniform(self.y_min, self.y_max)
         z = random.uniform(self.z_min, self.z_max)
         return (x, y, z)
 
     def convert_to_pixel_coordinates(self, x, y, z):
-        """
-        Converte coordenadas 3D (x, y, z) para coordenadas de pixel 2D.
-        
-        Argumentos:
-            x (float): Coordenada x.
-            y (float): Coordenada y.
-            z (float): Coordenada z.
-        
-        Retorna:
-            tuple: Um tuple contendo as coordenadas de pixel (px, py).
-        """
+
         # Normalizando as coordenadas
         x_normalized = (x - self.x_min) / (self.x_max - self.x_min)
         y_normalized = (y - self.y_min) / (self.y_max - self.y_min)
