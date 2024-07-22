43aaf2e7a81fa30dc82851e966e0a779dfd7f406
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-07-22 13:19:07 -0400

    Refatorar e melhorar o manuseio do botão em ButtonBorder.java
    - Adicionados novos campos para Button, WindowManager e MainActivity.
    - Atualizado o método setupRandomMoveOnClick para inicializar novos campos e melhorar a legibilidade.
    - Simplificada a lógica do onClick e removido código redundante.
    - Adicionado o método onCoordinatesReceived para lidar com coordenadas da interface e interagir com MainActivity.
    - Melhorados os logs e a legibilidade em toda a classe.
    - Removidas quebras de linha desnecessárias e declarações de log redundantes.

--- testAppMarvin/app/src/main/java/com/example/marvin/ButtonBorder.java
+++ testAppMarvin/app/src/main/java/com/example/marvin/ButtonBorder.java
@@ -1,6 +1,5 @@
 package com.example.marvin;
 
-
 import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
@@ -17,47 +16,41 @@ public class ButtonBorder {
     private static int buttonBorderY;
     private static int buttonBorderWidth;
     private static int buttonBorderHeight;
+    private static Button button;
+    private static WindowManager windowManager;
+    private static MainActivity mainActivity;
 
+    //Configura o comportamento do botão
+    public static void setupRandomMoveOnClick(final Button btn, final WindowManager wm, final Button replacementButton, MainActivity mainAct) {
+        button = btn;
+        windowManager = wm;
+        mainActivity = mainAct;
 
-
-
-    public static void setupRandomMoveOnClick(final Button button, final WindowManager windowManager, final Button replacementButton, MainActivity mainActivity) {
+        //Clique físico
         button.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-
                 clickCount++;
                 clickGeneralCount++;
 
-
-                if (clickCount == 2 && clickGeneralCount < 20 ) {
-                    // Obter as coordenadas após o movimento do botão
+                if (clickCount == 2 && clickGeneralCount < 20) {
                     int x = (int) button.getX();
                     int y = (int) button.getY();
                     String buttonTag = (String) button.getTag();
-
-
-                    // movimenta o botão
                     moveButtonRandomlyBorder(button, windowManager);
                     clickCount = 0;
-
-
                 } else if (clickCount < 2 && clickGeneralCount < 20) {
-                    // Obter as coordenadas após o movimento do botão
                     int x = (int) button.getX();
                     int y = (int) button.getY();
                     String buttonTag = (String) button.getTag();
-
-
-                } if (clickGeneralCount > 20) {
+                }
+                if (clickGeneralCount > 20) {
                     button.setVisibility(View.GONE);
                     replacementButton.setVisibility(View.VISIBLE);
                 }
-
             }
         });
-        // Adiciona OnLayoutChangeListener para monitorar mudanças no layout
+
         button.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
             @Override
             public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
@@ -67,7 +60,7 @@ public class ButtonBorder {
                 buttonBorderY = location[1];
                 buttonBorderWidth = right - left;
                 buttonBorderHeight = bottom - top;
-                Log.d("ButtonWandering", "Layout changed: X: " + buttonBorderX + ", Y: " + buttonBorderY + ", Width: " + buttonBorderWidth + ", Height: " + buttonBorderHeight);
+                Log.d("ButtonBorder", "Layout changed: X: " + buttonBorderX + ", Y: " + buttonBorderY + ", Width: " + buttonBorderWidth + ", Height: " + buttonBorderHeight);
             }
         });
 
@@ -75,71 +68,41 @@ public class ButtonBorder {
         button.invalidate();
     }
 
-
     private static void moveButtonRandomlyBorder(Button button, WindowManager windowManager) {
         DisplayMetrics displayMetrics = new DisplayMetrics();
         windowManager.getDefaultDisplay().getMetrics(displayMetrics);
         int screenWidth = displayMetrics.widthPixels;
-        Log.d("ButtonBorder", "Largura da tela: " + screenWidth);
         int screenHeight = displayMetrics.heightPixels;
-        Log.d("ButtonBorder", "Altura da tela: " + screenHeight);
         int buttonWidth = button.getWidth();
-        Log.d("ButtonBorder", "Largura do botão: " + buttonWidth);
         int buttonHeight = button.getHeight();
-        Log.d("ButtonBorder", "Altura do botão: " + buttonHeight);
-
-
         Random random = new Random();
 
-        // Escolhe uma borda aleatória: 0 para superior, 1 para direita, 2 para inferior, 3 para esquerda
         int edge = random.nextInt(4);
-        Log.d("ButtonBorder", "edge: " + edge);
-
         int randomX = 0;
         int randomY = 280; // Definindo a posição inicial de Y
 
         switch (edge) {
             case 0: // Borda superior
                 randomX = random.nextInt(900);
-                Log.d("ButtonBorder", "caso 0 X: " + randomX);
                 randomY = 0;
-                Log.d("ButtonBorder", "caso 0 Y: " + randomY);
-                Log.d("ButtonBorder", "Largura do botão: " + buttonWidth);
-                Log.d("ButtonBorder", "Altura do botão: " + buttonHeight);
-                // Define as coordenadas do botão
                 button.setX(randomX);
                 button.setY(randomY);
                 break;
             case 1: // Borda direita
                 randomX = 870;
                 randomY = random.nextInt(2000);
-                Log.d("ButtonBorder", "caso 1 X: " + randomX);
-                Log.d("ButtonBorder", "caso 1 Y: " + randomY);
-                Log.d("ButtonBorder", "Largura do botão: " + buttonWidth);
-                Log.d("ButtonBorder", "Altura do botão: " + buttonHeight);
-                // Define as coordenadas do botão
                 button.setX(randomX);
                 button.setY(randomY);
                 break;
             case 2: // Borda inferior
                 randomX = random.nextInt(900);
                 randomY = 500;
-                Log.d("ButtonBorder", "caso 2 X: " + randomX);
-                Log.d("ButtonBorder", "caso 2 Y: " + randomY);
-                Log.d("ButtonBorder", "Largura do botão: " + buttonWidth);
-                Log.d("ButtonBorder", "Altura do botão: " + buttonHeight);
-                // Define as coordenadas do botão
                 button.setX(randomX);
                 button.setY(randomY);
                 break;
             case 3: // Borda esquerda
                 randomX = 0;
-                Log.d("ButtonBorder", "caso 3 X: " + randomX);
                 randomY = random.nextInt(2050);
-                Log.d("ButtonBorder", "caso 3 Y: " + randomY);
-                Log.d("ButtonBorder", "Largura do botão: " + buttonWidth);
-                Log.d("ButtonBorder", "Altura do botão: " + buttonHeight);
-                // Define as coordenadas do botão
                 button.setX(randomX);
                 button.setY(randomY);
                 break;
@@ -149,10 +112,9 @@ public class ButtonBorder {
     }
 
     public static boolean isTouchOnButton(int x, int y) {
-        Log.d("MainActivity", "Button coordinates: x = " + buttonBorderX + ", y = " + buttonBorderY + ", width = "+ buttonBorderWidth + ", height= "+ buttonBorderHeight);
+        Log.d("MainActivity", "Button coordinates: x = " + buttonBorderX + ", y = " + buttonBorderY + ", width = " + buttonBorderWidth + ", height= " + buttonBorderHeight);
         Log.d("MainActivity", "Touch coordinates: x = " + x + ", y = " + y);
 
-        // Verifica se as coordenadas do toque estão dentro dos limites do botão
         boolean isOnButton = x >= buttonBorderX && x <= buttonBorderX + buttonBorderWidth &&
                 y >= buttonBorderY && y <= buttonBorderY + buttonBorderHeight;
 
@@ -161,7 +123,24 @@ public class ButtonBorder {
         return isOnButton;
     }
 
+    // Método para receber as coordenadas da interface
+    public static void onCoordinatesReceived(int x, int y, boolean inicio, int id_robot) {
+        Log.d("MainActivity", "Coordenadas recebidas: x = " + x + ", y = " + y);
 
-
+        if (isTouchOnButton(x, y)) {
+            Log.d("MainActivity", "As coordenadas atingiram o botão da borda");
+            mainActivity.enviarDadosParaServidor(true, id_robot); // Chama o método da MainActivity
+            clickCount++;
+            if (clickCount >= 2) {
+                moveButtonRandomlyBorder(button, windowManager); // Chama o método correto
+                clickCount = 0;
+            }
+        } else {
+            if (!inicio) {
+                mainActivity.enviarDadosParaServidor(false, id_robot); // Chama o método da MainActivity
+            }
+            Log.d("MainActivity", "As coordenadas não atingiram o botão");
+        }
+    }
 
 }
